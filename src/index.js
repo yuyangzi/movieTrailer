abdf4df603aa4764e136e2b8528cf52c517da904
wangyiming <wangyimig@icloud.com>
2018-03-07 00:34:35 +0800

    集成pug.js模板引擎渲染页面

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+const path = require('path');
+
 const Koa = require('koa');
+// 导入koa-views中间件, 管理模板引擎
+const koaViews = require('koa-views');
+
 
 // 导入模板文件
 const {normal} = require('../template/index');
@@ -6,9 +11,15 @@ const {normal} = require('../template/index');
 // new 一个Koa实例
 const app = new Koa();
 
-app.use(async (ctx) => {
-    ctx.type = 'text/html;';
-    ctx.body = normal;
+app.use(koaViews(path.resolve(__dirname, '../views'), {
+    extension: 'pug',
+}));
+
+app.use(async (ctx, next) => {
+    await ctx.render('index', {
+        you: '李白',
+        me: '王宜明'
+    })
 });
 
 // 指定端口
