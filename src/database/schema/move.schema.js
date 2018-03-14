348ad01b9c1356f111f3ee4f634108cc7ee20d5a
wangyiming <wangyimig@icloud.com>
2018-03-15 01:25:31 +0800

    设定move Schema 和 user Schema 且进行用户密码加盐操作

---
+++ src/database/schema/move.schema.js
@@ -0,0 +1,69 @@
+// 导入mongoose模块
+const mongoose = require('mongoose');
+
+// 拿到Schema构造函数.用以构建Schema
+const Schema = mongoose.Schema;
+
+// Mixed类型代表可以存储任何类型的数据
+const Mixed = Schema.Types.Mixed;
+
+// 创建move模型
+const MoveSchema = new Schema({
+    // 电影ID
+    moveId: {
+        type: String,
+        unique: true,
+    },
+    // 评分
+    rate: Number,
+    // 标题
+    title: String,
+    // 简介
+    summary: String,
+    // 视频
+    video: String,
+    // 海报图
+    poster: String,
+    // 封面图
+    cover: String,
+    // 图床上的地址
+    videoKey: String,
+    // 图床上的地址
+    posterKey: String,
+    // 图床上的地址
+    coverKey: String,
+    // 原始标题
+    rawTitle: String,
+    // 电影类型
+    moveTypes: [String],
+    // 上映日期
+    pubdate: Mixed,
+    // 上映年份
+    year: Number,
+    // 电影标签
+    tags: [String],
+    meta: {
+        // 数据创建时间
+        createAt: {
+            type: Date,
+            default: Date.now()
+        },
+        // 数据更新时间
+        updateAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+});
+
+// 在存储数据之前调用的函数
+MoveSchema.pre('save', next => {
+    if (this.isNew) {
+        this.meta.createAt = this.meta.updateAt = Date.now();
+    } else {
+        this.meta.updateAt = Date.now();
+    }
+    next();
+});
+
+mongoose.model('Move', MoveSchema);
